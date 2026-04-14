e7b93c7942224c17969e6d5338d236e88f320f62
Weizhou Zhou <141607395+just7n-zhou@users.noreply.github.com>
2026-04-13 20:45:52 -0700

    Add: course-schedule - submission-7

---
+++ Data Structures & Algorithms/course-schedule/submission-7.py
@@ -0,0 +1,23 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        inDegree = [0] * numCourses 
+        adj = [[] for _ in range(numCourses)]
+        for u, v in prerequisites:
+            adj[u].append(v)
+            inDegree[v] += 1
+        
+        queue = collections.deque()
+        for i in range(numCourses):
+            if inDegree[i] == 0:
+                queue.append(i)
+        
+        taken = 0
+        while queue:
+            u = queue.popleft()
+            taken += 1
+            for v in adj[u]:
+                inDegree[v] -= 1
+                if inDegree[v] == 0:
+                    queue.append(v)
+        
+        return taken == numCourses 
